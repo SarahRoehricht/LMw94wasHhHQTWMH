d9e2e7168f2082dfeec892c688427528c9934e5a
Sarah Roehricht <Sarah.Roehricht@Student.reutlingen-university.de>
2015-10-16 22:26:16 +0200

    Klasse Spiel - Variablen: spielBrett, Spieler[] - default constructor

---
+++ DAME/src/backend/Spiel.java
@@ -0,0 +1,14 @@
+package backend;
+
+public class Spiel implements iBediener{
+
+	
+	private SpielBrett spielbrett;
+	private static final int spielerMax = 2;
+	private Spieler[] spieler = new Spieler[spielerMax];
+	
+	
+	public Spiel(){
+
+	}
+}
