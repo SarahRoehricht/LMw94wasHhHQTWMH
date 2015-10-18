20477d0ea052aaa8c5ea7d988b8babb441b5aa3e
informatik <informatik@informatik>
2015-10-18 17:36:25 +0200

    Test mit array

--- DAME/src/backend/SpielBrett.java
+++ DAME/src/backend/SpielBrett.java
@@ -1,18 +1,37 @@
 package backend;
 
 public class SpielBrett {
-	private String Spielfelder;
+	private char[][] Spielfelder = new char [12][12];
+	// private String Spielfelder
 
-	SpielBrett(String Spielfelder) {
+	SpielBrett(char Spielfelder[][]) {
 		setSpielfelder(Spielfelder);
 	}
 
-	public String getSpielfelder() {
+	public char[][] getSpielfelder() {
 		return Spielfelder;
 	}
 
-	public void setSpielfelder(String spielfelder) {
+	public void setSpielfelder(char[][] spielfelder) {
+	
+	for (int i = 0; i < spielfelder[i].length; i++) {
+		Spielfelder[i]=A(i++);
+		for (int j = 0; j < spielfelder.length; j++) {
+			Spielfelder[i][j]=B(j++);
+		}
+		}
 		Spielfelder = spielfelder;
+		System.out.println(Spielfelder);
+	}
+
+	private char B(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private char[] A(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
