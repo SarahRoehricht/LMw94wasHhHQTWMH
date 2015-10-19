8a6d03e052a4b8930aaea0ad07c53fcbb1f86b79
KuKKilicious <clouseyda@yahoo.de>
2015-10-19 17:50:48 +0200

    Doch nochmal die Methode fillBrett umgeschrieben. erstellt jetzt ganzes Feld, und setzt Spielfeld farbe richtig. Konstruktor in Spielfeld angepasst. Ganzes Spielbrett ist nun printbar.

--- DAME/src/backend/SpielTest.java
+++ DAME/src/backend/SpielTest.java
@@ -11,9 +11,6 @@ public class SpielTest implements iBediener{
 	public static void main(String[] args) {
 	Spiel spiel= new Spiel();
 
-	Spielfeld c= new Spielfeld(2,3);
-	System.out.println();
-	System.out.println(c);
 	}
 
 }
