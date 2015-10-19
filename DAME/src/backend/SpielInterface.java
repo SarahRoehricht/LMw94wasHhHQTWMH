87b334c8e88a6df51bc9556cf23f2fd7fdd0ce76
informatik <informatik@informatik>
2015-10-19 09:09:23 +0200

    Bsp Interface hinzugefügt

---
+++ DAME/src/backend/SpielInterface.java
@@ -0,0 +1,7 @@
+package backend;
+
+public interface SpielInterface {
+
+	public void iBediener(Spielfeld Brett[][]); //BSp Interface dadurch müsste mann klasse Spielbrett mit implements SpielInterface initialisieren und dort die iBediener methode implemeintierung.
+	
+}
