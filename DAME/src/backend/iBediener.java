79a8ae07cf8f30b8e798fbf74d75c83e6c509f1a
Jonas29 <Jonas.Fiedler@Student.Reutlingen-University.de>
2015-10-26 09:11:59 +0100

    Interface vorhandene Methoden und anmerkung für später

--- DAME/src/backend/iBediener.java
+++ DAME/src/backend/iBediener.java
@@ -2,4 +2,9 @@ package backend;
 
 public interface iBediener {
 
+	 void add(Spieler s1);
+	 void welcome();
+	 int getSpielerBisher();
+	 void setSpielerBisher(int spielerBisher);
+	 void move(Spielfigur stein1, Spielfeld posxy); // mit vererbung über spiel ändern kein interface für 2 klassen.
 }
