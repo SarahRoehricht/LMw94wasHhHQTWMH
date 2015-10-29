2316603c7e7a5286aed8f4d3f75a53581f4caf9b
JoachimZerbig <Joachim.Zerbig@Student.Reutlingen-University.DE>
2015-10-29 15:21:39 +0100

    Jetzt gehts auch mit schwarz. Fehler bei weiß behoben. Brett auslesen ausgelagert.

--- DAME/src/backend/Ki.java
+++ DAME/src/backend/Ki.java
@@ -26,26 +26,48 @@ public abstract class Ki extends Spiel {
 		Random linksRechts = new Random();
 		int entscheide = linksRechts.nextInt(2);
 
+		//KopieSpielBrett
+		for (int i = 0; i < super.getSpielbrett().getBrett().length; i++) {
+			for (int j = 0; j < super.getSpielbrett().getBrett()[i].length; j++) {
+				spielbrett[i][j] = super.getSpielbrett().getBrett()[i][j];
+			}
+		}
+		
+		
+		//WEISS
 		if (super.getSpielerBisher() == 1) {
-			for (int i = 0; i < super.getSpielbrett().getBrett().length; i++) {
-				for (int j = 0; j < super.getSpielbrett().getBrett()[i].length; j++) {
-					spielbrett[i][j] = super.getSpielbrett().getBrett()[i][j];
+			for (int j = 0; j < spielbrett[4].length; j = j++) {
+				if (spielbrett[4][j % 2] != null) {
+					startWeiß[4][j] = spielbrett[4][j];
+					r = new Random();
+					int zufall = r.nextInt(startWeiß.length);
+					if (zufall == 0) {
+						super.move(spielbrett[4][0], spielbrett[5][1]);
+					} else {
+						super.move(spielbrett[4][zufall], spielbrett[5][zufall + lor[entscheide]]);
+					}
+
 				}
 			}
-
 		}
-		for (int j = 0; j < spielbrett[4].length; j = j++) {
-			if (spielbrett[4][j % 2] != null) {
-				startWeiß[4][j] = spielbrett[4][j];
-				r = new Random();
-				int zufall = r.nextInt(startWeiß.length);
-				if (zufall == 0) {
-					super.move(spielbrett[4][0], spielbrett[5][1]);
-				} else {
-					super.move(spielbrett[4][zufall], spielbrett[5][zufall + lor[entscheide]]);
-				}
+		
+		//SCHWARZ
+		if (super.getSpielerBisher() == 2) {
+			
+			for (int j = 1; j < spielbrett[7].length; j = j+2) {
+				if (spielbrett[7][j] != null) {
+					startWeiß[7][j] = spielbrett[7][j];
+					r = new Random();
+					int zufall = r.nextInt(startWeiß.length);
+					if (zufall == 11) {
+						super.move(spielbrett[7][11], spielbrett[6][10]);
+					} else {
+						super.move(spielbrett[7][zufall], spielbrett[6][zufall + lor[entscheide]]);
+					}
 
+				}
 			}
 		}
+
 	}
 }
