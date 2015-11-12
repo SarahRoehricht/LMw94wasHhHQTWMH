0c922039fb5d8b406648dd8bddf6816026ce3402
Jonas29 <Jonas.Fiedler@Student.Reutlingen-University.de>
2015-11-13 00:24:58 +0100

    Manueller Merge JLabel einführung uvm....

--- DAME/src/frontend/DameStartZielEingabe.java
+++ DAME/src/frontend/DameStartZielEingabe.java
@@ -1,14 +1,19 @@
 package frontend;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import backend.Spiel;
+
 
 public class DameStartZielEingabe {
 
@@ -29,14 +34,28 @@ public class DameStartZielEingabe {
 	String Startziel = "";
 	JPanel jp;
 	JFrame jf;
+	JButton jb;
+	JLabel jl;
 
 	public void JTFStartZiel() {
-		
+		jf = new JFrame();
 		jp = new JPanel();
 		jTF_Start_Ziel = new JTextField(7);
-		
+		jb = new JButton();
+		jl = new JLabel();
+		jf.add(jp);
 		jp.add(jTF_Start_Ziel);
-
+		jp.add(jb);
+		jp.add(jl);
+//		jf.setResizable(false);
+		jb.setForeground(Color.BLUE);
+			ImageIcon Icon = new ImageIcon("check.png");
+			ImageIcon Icon2 = new ImageIcon("error.png");
+			
+			//jb.setIcon(Icon);
+			
+//			jb.setSize(50,50);
+		
 		// jTF_Start_Ziel.getText();
 
 		// c.add(jTF_Start_Ziel, BorderLayout.EAST);
@@ -46,13 +65,29 @@ public class DameStartZielEingabe {
 		// jTF_Start_Ziel.addActionListener((ActionListener) Texthandler);
 		jTF_Start_Ziel.setSize(300, 100);
 		jTF_Start_Ziel.setVisible(true);
+		jTF_Start_Ziel.setSize(300, 100);
+		jTF_Start_Ziel.setVisible(true);
 //		jp.setVisible(true);
 //		jf.setSize(1920, 1080);
 //		jf.setVisible(true);
+		jb.setSize(100,50);
+		jb.setVisible(true);
+		jl.setSize(100,50);;
+		jl.setVisible(true);
 		jTF_Start_Ziel.addActionListener(new EH_DameStartZielEingabe(this));
+		jb.addActionListener(new EH_DameStartZielEingabe(this));
+		
 //		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
+
+//	public static void main(String[] args) {
+//		DameStartZielEingabe DEF = new DameStartZielEingabe();
+//
+//	}
+
+	
+	
 public Component getJPanel(){
 	return jp;
 }
--- DAME/src/frontend/EH_DameStartZielEingabe.java
+++ DAME/src/frontend/EH_DameStartZielEingabe.java
@@ -6,66 +6,75 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.regex.Pattern;
 
-public class EH_DameStartZielEingabe implements ActionListener{
+import javax.swing.ImageIcon;
+
+public class EH_DameStartZielEingabe implements ActionListener {
 	DameStartZielEingabe DEF = null;
+
 	public EH_DameStartZielEingabe(DameStartZielEingabe lol) {
 		this.DEF = lol;
-		
+
 	}
-	
+
 	ScrollPane sp;
-	
+	ImageIcon Icon = new ImageIcon("check.png");
+	ImageIcon Icon2 = new ImageIcon("error.png");
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String regel = "[a-lA-L]{1}[0-9]{1}[0-2]{0,1}-{1}[a-lA-L]{1}[0-9]{1}[0-2]{0,1}";
 		String ausgabe;
 		Font font;
-		font = new Font("ARIAL",Font.BOLD,14);
-		if (e.getSource() == DEF.jTF_Start_Ziel ) {
+		font = new Font("ARIAL", Font.BOLD, 14);
+		DEF.jl.setText("");
+		if (e.getSource() == DEF.jTF_Start_Ziel || e.getSource() == DEF.jb) {
 			DEF.jTF_Start_Ziel.setForeground(Color.black);
 			DEF.jTF_Start_Ziel.setFont(font);
 			DEF.jTF_Start_Ziel.setBackground(Color.white);
 			if (Pattern.matches(regel, DEF.jTF_Start_Ziel.getText())) {
 				String[] sz = new String[2];
 				sz = DEF.jTF_Start_Ziel.getText().split("-", 2);
-				//System.out.println(sz[0]+","+sz[1]);
-				//System.out.println(DEF.spiel.getSpieler()[1]);
-				if ((DEF.spiel.doTheMove(DEF.spiel.getActiveSpieler(), DEF.spiel.EingabeSpielfeld(sz[0]),
-						DEF.spiel.EingabeSpielfeld(sz[1])) == false)) {
-					//System.out.println("Falscher Zug bzw Zug nicht möglich erneute eingabe");
-					ausgabe= "Falsch Synatax";
-					//sp.addToTextArea(ausgabe);
-					
-					
+				
+				if ((DEF.spiel.doTheMove(DEF.spiel.getActiveSpieler(), DEF.spiel.EingabeSpielfeld(sz[0]), DEF.spiel.EingabeSpielfeld(sz[1])) == false)) {
+					// System.out.println("Falscher Zug bzw Zug nicht möglich erneute eingabe");
+					ausgabe = DEF.jTF_Start_Ziel.getText()+"ist kein gültiger Zug";
+					// sp.addToTextArea(ausgabe);
+					DEF.jl.setForeground(Color.red);
+					DEF.jl.setText(DEF.jTF_Start_Ziel.getText()+" ist kein gültiger Zug");
 					DEF.jTF_Start_Ziel.setFont(font);
 					DEF.jTF_Start_Ziel.setBackground(Color.red);
-					
-				}
-				else {
-					
-					
-					ausgabe= sz[0]+"-"+sz[1];
-					//System.out.println(ausgabe);
-					//sp.addToTextArea(ausgabe);
+					DEF.jTF_Start_Ziel.setForeground(Color.white);
+				//	DEF.jb.setIcon(Icon2);
+
+					// DEF.jb.setIcon(Icon);
+				} else {
+					DEF.jl.setForeground(Color.green);
+					DEF.jl.setText(DEF.spiel.getActiveSpieler()+" hat von "+sz[0]+" nach "+sz[1]+ " gezogen");
+					ausgabe = sz[0] + "-" + sz[1];
+					// System.out.println(ausgabe);
+					// sp.addToTextArea(ausgabe);
 					DEF.jTF_Start_Ziel.setText("");
+					//if((DEF.spiel.zugdone)==true)
+					//DEF.jb.setIcon(Icon);
 				}
 			} else {
-				
-				ausgabe="Nicht möglicher Zug";
+
+				ausgabe = "Eingabe Syntax nicht korrekt, Feld nicht auf Spielbrett";
+				//DEF.jb.setIcon(Icon2);
+				DEF.jl.setForeground(Color.red);
+				DEF.jl.setText("Eingabe Syntax nicht korrekt, Feld nicht auf Spielbrett");
 				DEF.jTF_Start_Ziel.setFont(font);
 				DEF.jTF_Start_Ziel.setBackground(Color.red);
 				DEF.jTF_Start_Ziel.setForeground(Color.white);
-				//sp.addToTextArea(ausgabe);
-				//System.out.println("Falsche eingabe neue eingabe");// an unteres
-																	// Textfeld
-																	// weiterleiten
+
+				// sp.addToTextArea(ausgabe);
+				// System.out.println("Falsche eingabe neue eingabe");// an unteres
+				// Textfeld
+				// weiterleiten
 				// eingabe erneut starten...
 			}
+
 		}
 	}
 
-	
-	
-	
-	
 }
